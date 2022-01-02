5a7f58f9ee66d259220aa3ff351200634b0136c3
Caroline Knote <cj.knote@gmail.com>
2022-01-01 23:15:06 -0800

    changing to use ajax requests to fufill

--- tvmaze.js
+++ tvmaze.js
@@ -18,21 +18,23 @@
       }
  */
 async function searchShows(query) {
-  // TODO: Make an ajax request to the searchShows api.  Remove
-  // hard coded data.
 
+  //request show list from the api, wait for response.
   let response = await axios.get(`https://api.tvmaze.com/search/shows?q=${query}`);
   
-  console.log(response);
+  //create an array of objects with the information we need from each show. (mapping so we have the info for each show returned)
+  let shows = response.data.map(listing => {
+    let show = listing.show;
+    return{
+      id: show.id,
+      name: show.name,
+      summary: show.summary,
+      //if there is a show image, get the url (show.image.medium), otherwise use placeholder image.
+      image: show.image ? show.image.medium: 'https://tinyurl.com/tv-missing'
+    };
+  });
 
-  // return [
-  //   {
-  //     id: 1767,
-  //     name: "The Bletchley Circle",
-  //     summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-  //     image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-  //   }
-  // ]
+  return shows;
 }
 
 
@@ -42,17 +44,22 @@ async function searchShows(query) {
  */
 
 function populateShows(shows) {
+  //create a reference to the shows list area
   const $showsList = $("#shows-list");
+  //clear the area before repopulating
   $showsList.empty();
 
+  //create a DOM card for every show in the list
   for (let show of shows) {
     let $item = $(
       `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
          <div class="card" data-show-id="${show.id}">
+           <img class="card-img-top" src="${show.image}">
            <div class="card-body">
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
            </div>
+           <button id="episode-btn" class="episode-btn">Show Episodes</button>
          </div>
        </div>
       `);
@@ -86,9 +93,51 @@ $("#search-form").on("submit", async function handleSearch (evt) {
  */
 
 async function getEpisodes(id) {
-  // TODO: get episodes from tvmaze
-  //       you can get this by making GET request to
-  //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
 
-  // TODO: return array-of-episode-info, as described in docstring above
+  //request info from the api for the selected id, wait for the response.
+  let response = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
+  
+  //creates array of objects from the episode array from the api with the info we need to use
+  let episodes = response.data.map(episode => ({
+    id: episode.id,
+    name: episode.name,
+    season: episode.season,
+    number: episode.number
+  }));
+  
+  //returns object list
+  return episodes;
 }
+
+//add the episodes to the DOM
+function populateEpisodes(episodeArr){
+  //creates reference for the episode list section of the DOM
+  let $episodeList = $('#episodes-list');
+  //clears the info that was in the area previously, so we only show most recently clicked show info
+  $episodeList.empty();
+
+  //create the list item for each episode and add it to the list
+  for (let ep of episodeArr){
+    //create a li with the episode information
+    let $li = $("<li>").text(`${ep.name} (Season ${ep.season}, Episode ${ep.number})`);
+
+    //add the li to the list
+    $episodeList.append($li);
+  }
+
+  //unhide the episode area
+  $('#episodes-area').show();
+
+}
+
+//handle clicks on the show Episode button
+$("#shows-list").on("click", ".card", async function buttonClick(e){
+  //gets the closest card to the clicked button
+  let showId = $(e.target).closest(".card").data("show-id");
+
+  //fetches the episode list and waits for it to finish
+  let episodes = await getEpisodes(showId);
+  //once episode list is recieved, display the episodes
+  populateEpisodes(episodes);
+
+});
